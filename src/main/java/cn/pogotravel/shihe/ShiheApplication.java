8c33a6ed0aec45abfaa82407ce1cc00766a68139
unknown <653031404@qq.com>
2019-09-27 15:05:58 +0800

    init commit

---
+++ src/main/java/cn/pogotravel/shihe/ShiheApplication.java
@@ -0,0 +1,13 @@
+package cn.pogotravel.shihe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/pogotravel/shihe/ShiheApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.pogotravel.shihe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiheApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
