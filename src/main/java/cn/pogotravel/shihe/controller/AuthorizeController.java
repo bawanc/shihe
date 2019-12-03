7194181eb9106bd82efd41df021c337558dc6d24
baqian <653031404@qq.com>
2019-12-03 13:01:44 +0800

    add mysql name

--- src/main/java/cn/pogotravel/shihe/controller/AuthorizeController.java
+++ src/main/java/cn/pogotravel/shihe/controller/AuthorizeController.java
@@ -2,36 +2,59 @@ package cn.pogotravel.shihe.controller;
 
 import cn.pogotravel.shihe.dto.AccessTokenDTO;
 import cn.pogotravel.shihe.dto.GithubUserDTO;
+import cn.pogotravel.shihe.mapper.UserMapper;
+import cn.pogotravel.shihe.model.User;
 import cn.pogotravel.shihe.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-    @Value("github.client.id")
-    private String clientid;
-    @Value("github.client.secret")
-    private String clientsecret;
-    @Value("github.redirect.url")
-    private String redirecturl;
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state ){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state ,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id(clientid);
-        accessTokenDTO.setClient_secret(clientsecret);
-        accessTokenDTO.setRedirect_url(redirecturl);
+        accessTokenDTO.setRedirect_url(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUserDTO githubUserDTO = githubProvider.gitUser(accessToken);
-        System.out.println(githubUserDTO.getName());
-
-        return "index";
+        if (githubUserDTO != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUserDTO.getName());
+            user.setAccountId(String.valueOf(githubUserDTO.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUserDTO);
+            return "redirect:/";
+        }
+        return "redirect:/";
     }
 }
